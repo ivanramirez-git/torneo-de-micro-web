7935e8ac34fb604db5eaaa8b296badf98e41909a
ivanramirez-git <ivanrene10@gmail.com>
2024-11-22 06:13:28 -0500

    a

--- src/stores/api.ts
+++ src/stores/api.ts
@@ -1,8 +1,22 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://api-micro.freeloz.com'
-});
+const api = axios.create({ baseURL: 'https://api-micro.freeloz.com' });
+
+// Add a request interceptor
+api.interceptors.request.use(
+  config => {
+    // Si la ruta de la solicitud es /visitas y el método no es GET, se envía el token
+    if (config.url == '/visitas') return config;
+    if (config.method == 'get') return config;
+
+    const token = localStorage.getItem('token');
+    if (token) config.headers['Authorization'] = `Bearer ${token}`;
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 // Add a response interceptor
 api.interceptors.response.use(
@@ -16,11 +30,5 @@ api.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-
-// Add token to requests if it exists
-const token = localStorage.getItem('token');
-if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-}
 
 export default api;
